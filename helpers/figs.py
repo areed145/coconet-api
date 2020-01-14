c8f439b32fc790cecd4cde141b06bbacce3744c4
areed145 <areed145@gmail.com>
2020-01-13 22:49:15 -0600

    add boundaries

--- helpers/figs.py
+++ helpers/figs.py
@@ -1281,7 +1281,7 @@ def create_map_awc(prop, lat=38, lon=-96, zoom=3, stations='1', infrared='0', ra
         layers.append(
             dict(
                 below='traces',
-                opacity=0.5,
+                # opacity=0.5,
                 sourcetype='raster',
                 source=[
                     'https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/sat_meteo_imagery_time/MapServer/export?transparent=true&format=png32&dpi=300&layers=show:20,8&bbox={bbox-epsg-3857}&bboxSR=3857&imageSR=3857&f=image']
@@ -1291,10 +1291,20 @@ def create_map_awc(prop, lat=38, lon=-96, zoom=3, stations='1', infrared='0', ra
         layers.append(
             dict(
                 below='traces',
-                opacity=0.5,
+                # opacity=0.5,
                 sourcetype='raster',
                 source=[
                     'https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/sat_meteo_imagery_time/MapServer/export?transparent=true&format=png32&dpi=300&layers=show:16&bbox={bbox-epsg-3857}&bboxSR=3857&imageSR=3857&f=image']
+            )
+        )
+    if visible == '1' or infrared == '1':
+        layers.append(
+            dict(
+                below='traces',
+                # opacity=0.5,
+                sourcetype='raster',
+                source=[
+                    'https://services.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer/export?transparent=true&format=png32&dpi=300&layers=show:16&bbox={bbox-epsg-3857}&bboxSR=3857&imageSR=3857&f=image'
             )
         )
     if radar == '1':
