7cd9b9a8995348cb292c575e152e297ff64e7ceb
areed145 <areed145@gmail.com>
2020-01-19 17:27:24 -0600

    updarte

--- app.py
+++ app.py
@@ -61,7 +61,6 @@ async def aprs_latest():
     last = figs.get_aprs_latest()
     data = {}
     data['last'] = last
-    print(data)
     json_compatible_item_data = jsonable_encoder(data)
     return JSONResponse(content=json_compatible_item_data)
 
