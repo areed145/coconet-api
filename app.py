765543dfcc186416a96d6531829cf0c318198741
areed145 <areed145@gmail.com>
2020-02-24 18:38:51 -0600

    update

--- app.py
+++ app.py
@@ -48,12 +48,12 @@ def myconverter(o):
         return o.__str__()
 
 
-@app.get('/')
+@app.get('/', tags=['status'])
 def main():
-    return {'api': 'kk6gpv.net'}
+    return {'status': 'active'}
 
 
-@app.get('/aprs/latest')
+@app.get('/aprs/latest', tags=['aprs', 'latest'])
 async def aprs_latest():
     last = aprs.get_aprs_latest()
     data = {}
@@ -62,7 +62,7 @@ async def aprs_latest():
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/aprs/map')
+@app.get('/aprs/map', tags=['aprs', 'map'])
 async def aprs_map(type_aprs: str, prop_aprs: str, time_int: str):
     map_aprs, plot_speed, plot_alt, plot_course, rows = aprs.create_map_aprs(
         type_aprs, prop_aprs, time_int)
@@ -76,7 +76,7 @@ async def aprs_map(type_aprs: str, prop_aprs: str, time_int: str):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/aprs/igate_range')
+@app.get('/aprs/igate_range', tags=['aprs', 'graph'])
 async def aprs_igate_range(time_int: str):
     range_aprs = aprs.create_range_aprs(time_int)
     data = {}
@@ -85,7 +85,7 @@ async def aprs_igate_range(time_int: str):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/iot/graph')
+@app.get('/iot/graph', tags=['iot', 'graph'])
 async def iot_graph(time_int: str, sensor_iot: List[str] = Query(None)):
     graph = iot.create_graph_iot(sensor_iot, time_int)
     data = {}
@@ -97,7 +97,7 @@ async def iot_graph(time_int: str, sensor_iot: List[str] = Query(None)):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/iot/anomaly')
+@app.get('/iot/anomaly', tags=['iot', 'anomaly'])
 async def iot_anomaly(time_int: str, sensor_iot: str):
     graph, anomaly, spectro = iot.create_anomaly_iot(sensor_iot, time_int)
     data = {}
@@ -111,7 +111,7 @@ async def iot_anomaly(time_int: str, sensor_iot: str):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/iot/spectrogram')
+@app.get('/iot/spectrogram', tags=['iot', 'graph'])
 async def iot_spectro(time_int: str, sensor_iot: str):
     graph, spectro = iot.create_spectrogram_iot(sensor_iot, time_int)
     data = {}
@@ -124,7 +124,7 @@ async def iot_spectro(time_int: str, sensor_iot: str):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/oilgas/tags/get')
+@app.get('/oilgas/tags/get', tags=['oilgas', 'tags'])
 async def oilgas_tags_get(api: str):
     tags = oilgas.get_tags_oilgas(str(api))
     try:
@@ -140,13 +140,13 @@ async def oilgas_tags_get(api: str):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.put('/oilgas/tags/set')
+@app.put('/oilgas/tags/set', tags=['oilgas', 'tags'])
 async def oilgas_tags_set(api: str, tags: List[str] = Query(None)):
     oilgas.set_tags_oilgas(api, tags)
     return dict(result='success')
 
 
-@app.get('/oilgas/header/tags')
+@app.get('/oilgas/header/tags', tags=['oilgas', 'tags'])
 async def oilgas_header_tags(tags: List[str] = Query(None)):
     headers = oilgas.get_header_tags_oilgas(tags)
     data = {}
@@ -158,7 +158,7 @@ async def oilgas_header_tags(tags: List[str] = Query(None)):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/oilgas/header/details')
+@app.get('/oilgas/header/details', tags=['oilgas', 'header'])
 async def oilgas_header_details(api: str):
     header = oilgas.get_header_oilgas(str(api))
     data = {}
@@ -170,7 +170,7 @@ async def oilgas_header_details(api: str):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/oilgas/prodinj/graph')
+@app.get('/oilgas/prodinj/graph', tags=['oilgas', 'production', 'graph'])
 async def oilgas_prodinj_graph(api: str, axis: str):
     graph_oilgas = oilgas.get_graph_oilgas(str(api), axis)
     data = {}
@@ -182,12 +182,13 @@ async def oilgas_prodinj_graph(api: str, axis: str):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.put('/oilgas/decline/solve')
+@app.put('/oilgas/decline/solve', tags=['oilgas', 'reservoir'])
 async def oilgas_tags_set(api: str):
     oilgas.set_decline_oilgas(api)
     return dict(result='success')
 
 
+@app.get('/oilgas/decline/graph', tags=['oilgas', 'reservoir', 'graph'])
 async def oilgas_decline_graph(api: str, axis: str):
     graph_decline, graph_decline_cum = oilgas.get_decline_oilgas(
         str(api), axis)
@@ -201,21 +202,7 @@ async def oilgas_decline_graph(api: str, axis: str):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/oilgas/decline/graph')
-async def oilgas_decline_graph(api: str, axis: str):
-    graph_decline, graph_decline_cum = oilgas.get_decline_oilgas(
-        str(api), axis)
-    data = {}
-    try:
-        data['graph_decline'] = json.loads(graph_decline)
-        data['graph_decline_cum'] = json.loads(graph_decline_cum)
-    except:
-        pass
-    json_compatible_item_data = jsonable_encoder(data)
-    return JSONResponse(content=json_compatible_item_data)
-
-
-@app.get('/oilgas/crm/graph')
+@app.get('/oilgas/crm/graph', tags=['oilgas', 'reservoir', 'graph'])
 async def oilgas_crm_graph(api: str):
     graph_crm = oilgas.get_crm(str(api))
     data = {}
@@ -227,7 +214,7 @@ async def oilgas_crm_graph(api: str):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/oilgas/cyclic/graph')
+@app.get('/oilgas/cyclic/graph', tags=['oilgas', 'production', 'graph'])
 async def oilgas_cyclic_graph(api: str):
     graph_cyclic_jobs = oilgas.get_cyclic_jobs(str(api))
     data = {}
@@ -239,7 +226,7 @@ async def oilgas_cyclic_graph(api: str):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/oilgas/offset/graphs')
+@app.get('/oilgas/offset/graphs', tags=['oilgas', 'production', 'graph'])
 async def oilgas_offset_graph(api: str, axis: str):
     graph_offset_oil, graph_offset_stm, graph_offset_wtr, graph_offset_oil_ci, graph_offset_stm_ci, graph_offset_wtr_ci, map_offsets, offsets = oilgas.get_offsets_oilgas(
         str(api), radius=0.1, axis=axis)
@@ -272,7 +259,7 @@ async def oilgas_offset_graph(api: str, axis: str):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/photos/galleries')
+@app.get('/photos/galleries', tags=['photos'])
 async def photos_galleries():
     rows = flickr.get_gal_rows(5)
     data = {}
@@ -281,7 +268,7 @@ async def photos_galleries():
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/photos/gallery')
+@app.get('/photos/gallery', tags=['photos'])
 async def photos_gallery(id: str):
     rows, map_gal, title, count_photos, count_views = flickr.get_photo_rows(
         id, 5)
@@ -295,7 +282,7 @@ async def photos_gallery(id: str):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/photos/photo')
+@app.get('/photos/photo', tags=['photos'])
 async def photos_photo(id: str):
     image, map_photo = flickr.get_photo(id)
     data = {}
@@ -308,7 +295,7 @@ async def photos_photo(id: str):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/station/history/graphs')
+@app.get('/station/history/graphs', tags=['weather', 'graph'])
 async def station_history_graphs(time_int: str):
     fig_td, fig_pr, fig_cb, fig_pc, fig_wd, fig_su, fig_wr, fig_thp = weather.create_wx_figs(
         time_int, sid)
@@ -325,7 +312,7 @@ async def station_history_graphs(time_int: str):
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/station/live/data')
+@app.get('/station/live/data', tags=['weather', 'latest'])
 async def station_live_data():
     wx = weather.get_wx_latest(sid)
     data = {}
@@ -334,7 +321,7 @@ async def station_live_data():
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/weather/aviation/map')
+@app.get('/weather/aviation/map', tags=['weather', 'map'])
 async def weather_aviation_map(
     prop_awc: str = 'flight_category',
     lat: float = 29.78088,
@@ -358,7 +345,7 @@ async def weather_aviation_map(
     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get('/weather/soundings/image')
+@app.get('/weather/soundings/image', tags=['weather', 'image'])
 async def weather_soundings_images(sid: str):
     img = weather.get_image(sid)
     json_compatible_item_data = jsonable_encoder(img.decode('unicode_escape'))
